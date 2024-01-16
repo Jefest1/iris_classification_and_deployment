41e02eed2b00d6fd187a03414e24b234d2fb0008
Jefest1 <anyanf101@gmail.com>
2024-01-16 21:12:56 +0000

    updated: fucntion to recieve a file and make predictions

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
 import joblib
 import numpy as np
+
+
 app = Flask(__name__)
 
 
 @app.route('/predict', methods=['POST', 'GET'])
 #  Creating a predict fucntion
 def predict():
-
     # Getting the data
     s_length = request.args.get('s_length')
     s_width = request.args.get('s_width')
@@ -25,6 +27,23 @@ def predict():
     # predicting the request
     pred = model.predict(data)
     return str(pred)
+
+
+@app.route('/predict_file', methods=['POST', 'GET'])
+def predict_file():
+    # Get the file
+    file = pd.read_csv(request.files.get('test.csv'), header=None)
+
+    # scale the data
+    scaler = StandardScaler()
+    file = scaler.fit_transform(file)
+
+    # predict the types of flowers
+    model = joblib.load('model.pkl')
+    pred = model.predict(file)
+
+    # return the result
+    return jsonify({'prediction': pred.tolist()})
 
 
 if __name__ == '__main__':
